acb3b427928811b4a46d9251b22014f5c23b334e
AshenSachinthana <dasachinthana@gmail.com>
2024-02-14 22:58:14 +0530

    Added receptionist controller

--- app/controllers/Receptionist.php
+++ app/controllers/Receptionist.php
@@ -3,58 +3,58 @@
         private $auth;
         public function __construct(){
             $this->auth = new AuthMiddleware();
-            $this->auth->authMiddleware('Receptionist');
+            $this->auth->authMiddleware('receptionist');
         }
 
         public function index(){
 
             $data = [];
-            $this->view("Receptionist/dashboard" , $data);
+            $this->view("receptionist/dashboard" , $data);
         }
 
         public function register(){
 
             $data = [];
-            $this->view("Receptionist/register" , $data);
+            $this->view("receptionist/register" , $data);
         }
 
         public function appointment(){
 
             $data = [];
-            $this->view("Receptionist/appointment" , $data);
+            $this->view("receptionist/appointment" , $data);
         }
 
         public function dashboard(){
 
             $data = [];
-            $this->view("Receptionist/dashboard" , $data);
+            $this->view("receptionist/dashboard" , $data);
         }
 
         public function profile(){
 
             $data = [];
-            $this->view("Receptionist/profile" , $data);
+            $this->view("receptionist/profile" , $data);
         }
 
         public function payment(){
 
             $data = [];
-            $this->view("Receptionist/payment" , $data);
+            $this->view("receptionist/payment" , $data);
         }
         public function patient_form(){
 
             $data = [];
-            $this->view("Receptionist/patient_form" , $data);
+            $this->view("receptionist/patient_form" , $data);
         }
         public function appointment_form(){
 
             $data = [];
-            $this->view("Receptionist/appointment_form" , $data);
+            $this->view("receptionist/appointment_form" , $data);
         }
         public function payment2_form(){
 
             $data = [];
-            $this->view("Receptionist/payment2_form" , $data);
+            $this->view("receptionist/payment2_form" , $data);
         }
     }
 ?>
