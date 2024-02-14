05b28c66b8708e02a1a44898aa737dbfa3185114
AshenSachinthana <dasachinthana@gmail.com>
2024-02-15 01:17:51 +0530

    medical reports view

--- app/views/mlt/reports.php
+++ app/views/mlt/reports.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta http-equiv="refresh" content="600; url=http://localhost/labora/user/logout"> -->
-    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/mlt/report2.css'?>">
+    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/mlt/report.css'?>">
     <script src="<?php echo APPROOT.'/public/js/mlt/mlt.js';?>"></script>
     <!-- static icons -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
