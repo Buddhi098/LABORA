1839d0969b6ac6e54b988dfc2667c54fab64d696
AshenSachinthana <dasachinthana@gmail.com>
2024-02-17 15:30:52 +0530

    add user

--- app/views/admin/userAccount.php
+++ app/views/admin/userAccount.php
@@ -17,148 +17,79 @@
     <?php require_once 'components/nevbar.php' ?>
     <div class="container_1">
 
-        <div class="tablename">
-            <h3>User Accounts</h3>
+    <div class="table-container">
+        <h2><i class="fa-solid fa-calendar-check"></i>User Accounts</h2>
+        <div class="add">
+            <a href="<?php echo URLROOT?>admin/addUser" class="addbtn"><ion-icon name="add"></ion-icon> Add Employee</a>
         </div>
-        <div class="line"></div>
-        <div class="searchbar">
-            <input type="text" class="search" placeholder="Enter name">
-            <a href="#" class="searchbtn">Search</a>
+        <div class="search-container">
+        <input type="text" class="search-box" id="searchInput" placeholder="Search...">
+        <button class="search-button">Search</button>
         </div>
 
-        <div class="filter">
-            <form action="#" method="post">
-                <!-- <input type="text" class="test-type" placeholder="Enter test type"> -->
-                <select type="text" class="test-type">
-                    <option value="option1">All</option>
-                    <option value="option2">Supplier</option>
-                    <option value="option3">MLT</option>
-                    <option value="option2">Lab Assistant</option>
-                    <option value="option2">Inventory Manager</option>
-                    <option value="option2">Receptionist</option>
-
+        <div class="filter-box">
+            <div class="filter-section">
+                <select class="filter-box">
+                <option value="all">All</option>
+                <option value="category1">Category 1</option>
+                <option value="category2">Category 2</option>
                 </select>
-                <!-- <input type="date" class="from">
-                <input type="date" class="to"> -->
-                <button type="submit" class="submit button">Filter</button>
-            </form>
+                <button class="filter-button">Filter By ID</button>
+            </div>
+            <div class="filter-section">
+                <select class="filter-box">
+                <option value="all">All</option>
+                <option value="category1">Category 1</option>
+                <option value="category2">Category 2</option>
+                </select>
+                <button class="filter-button">Filter By Email</button>
+            </div>
         </div>
-        <div class="add">          
-            <a href="http://localhost/labora/patientdashboard/user_form" class="addbtn"><ion-icon name="add"></ion-icon> Add User</a>
-         </div>
-         
-        <table>
+        <table id="myTable">
             <thead>
-                <tr>
                     <th>Index</th>
-                    <th>User Id</th>
-                    <th>Name</th>
-                    <th>Role</th>
-                    <th>Mobile</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>MLT123</td>
-                    <td>John Silva</td>
-                    <td>MLT</td>
-                    <td>071-1234567</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>LA1-456</td>
-                    <td>Lisa Perera</td>
-                    <td>Lab Assistant 1</td>
-                    <td>077-9876543</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>LA2-789</td>
-                    <td>Mark Fernando</td>
-                    <td>Lab Assistant 2</td>
-                    <td>076-5432109</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>INV456</td>
-                    <td>Susan Rajapakse</td>
-                    <td>Inventory Manager</td>
-                    <td>072-4567890</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>REC123</td>
-                    <td>Mike Wijesinghe</td>
-                    <td>Receptionist</td>
-                    <td>071-9876543</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>SUPP1</td>
-                    <td>Linda Perera</td>
-                    <td>Supplier 1</td>
-                    <td>075-1234567</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>7</td>
-                    <td>SUPP2</td>
-                    <td>David Fernando</td>
-                    <td>Supplier 2</td>
-                    <td>076-9876543</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>8</td>
-                    <td>SUPP3</td>
-                    <td>Susan Silva</td>
-                    <td>Supplier 3</td>
-                    <td>070-5432109</td>
-                    <!-- Add actions or links for this row -->
-                    <td>
-                        <a href="#" class="download"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="delete"><ion-icon name="trash"></ion-icon></a>
-                    </td>
-                </tr>
-                <!-- You can add more rows with similar data as needed -->
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Date Of Birth</th>
+                    <th>Address</th>
+                    <th>gender</th>
+                    <th>role</th>
+                    <th></th>
+            </thead >
+        <tbody>
+                <div class='table_body'>
+                <?php
+                
+                $reversedArray = array_reverse($data , true);
+                if(count($reversedArray)>1){
+                    foreach ($reversedArray as $row) {
+                        echo '<tr>
+                        <td>'.$row['id'].'</td>
+                        <td>'.$row['full_name'].'</td>
+                        <td>'.$row['email'].'</td>
+                        <td>'.$row['phone'].'</td>
+                        <td>'.$row['dob'].'</td>
+                        <td>'.$row['address'].'</td>
+                        <td>'.$row['gender'].'</td>
+                        <td>'.$row['role'].'</td>
+                        <td><a href="#" class="action-button">Edit</a> <a href="http://localhost/labora/admin/deleteEmployee/'.$row['email'].'" class="action-button">Delete</a></td>
+                        </tr>';
+                    }
+                }
+                ?>
+                </div>
             </tbody>
         </table>
+            <div class="pagination">
+            <h5 id="table_data"></h5>
+            <button onclick="previousPage()" >Previous</button>
+            <button onclick="nextPage()" id="next">Next</button>
+            </div>
         </div>
     </div>
+
+    <!-- import table javascript -->
+    <script src="<?php echo APPROOT.'/public/js/components/table.js'?>"></script>
 </body>
 </html>
