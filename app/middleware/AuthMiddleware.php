4c2440322b8ea269b6372268b9a5b5fec86bec68
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-02-18 14:52:28 +0530

    update authmiddleware

--- app/middleware/AuthMiddleware.php
+++ app/middleware/AuthMiddleware.php
@@ -5,10 +5,11 @@ class AuthMiddleware {
     public function __construct() {
     }
     public function authMiddleware($userRole) {
-        if(isset($_SESSION['role']) && $_SESSION['role'] == $userRole && (time() - $_SESSION['last_login_timestamp']) < 600){
+        if(isset($_SESSION['role']) && $_SESSION['role'] == $userRole && (time() - $_SESSION['last_login_timestamp']) < 10000){
             $_SESSION['last_login_timestamp'] = time();
             return true;
         }else {
+            
             header("Location: http://localhost/labora/user/logout");
             exit();
         }
