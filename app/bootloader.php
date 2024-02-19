e73e73a5f68c7607d73a92458b6ca3e11ef57925
Buddhi Nadeeshan <127881398+Buddhi098@users.noreply.github.com>
2024-02-19 21:52:54 +0530

    Revert "Buddhi"

--- app/bootloader.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'helpers/session_helper.php';
-    require_once 'helpers/send_email_helper.php';
-    require_once 'config/config.php';
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
-    require_once 'middleware/AuthMiddleware.php'
-?>
