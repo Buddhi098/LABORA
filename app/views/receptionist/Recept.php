d8815a8bd83a51949487714a04db796ffa50bc95
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-04-04 21:23:03 +0530

    update recept view

---
+++ app/views/receptionist/Recept.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="refresh" content="600; url=http://localhost/labora/user/logout"> -->
+    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/receptdashboard/recept.css'?>">
+    <script src="<?php echo APPROOT.'/public/js/receptdashboard/recept.js';?>"></script>
+    <!-- static icons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- annimation icons -->
+    <script src="https://cdn.lordicon.com/lordicon-1.1.0.js"></script>
+    <title>Receptionist dashboard</title>
+</head>
+<body>
+    <?php require_once 'components/nevbar.php' ?>
+</body>
+</html>
