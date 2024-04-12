10916da119c70f561d825cc51560c5b56bfb1416
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-04-13 02:43:20 +0530

    add qrcode view

---
+++ app/views/receptionist/invalid_qrcode.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/receptionist/invalid_qrcode.css'?>">
+  <title>Invalid Appointment</title>
+</head>
+<body>
+  <div class="error-container">
+    <div class="error-message">
+      <i class="fas fa-times-circle error-icon"></i>
+      <h2>Invalid QR Code</h2>
+      <p>The QR code you provided is not valid. Please try again.</p>
+    </div>
+  </div>
+</body>
+</html>
