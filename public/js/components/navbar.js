f4b0c0e18a09263a99be0ea8e4149512b9384647
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-04-13 11:54:58 +0530

    update nevbar js

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -1,5 +1,7 @@
 // common functions for some functionality
 
+// window.onload = setHovered();
+
 function sanitize(string) {
     const map = {
         '&': '&amp;',
@@ -12,6 +14,36 @@ function sanitize(string) {
     const reg = /[&<>"'/]/ig;
     return string.replace(reg, (match)=>(map[match]));
 }
+
+// // add hovered class to selected list item
+// let list = document.querySelectorAll(".navigation li");
+
+// function setHovered() {
+//   let list = document.querySelectorAll(".navigation li");
+//   console.log(list);
+//   console.log("setting hovered");
+//   let activeLinkid = sessionStorage.getItem("activeLinkID");
+//   console.log(activeLinkid);
+//   let activeLink = document.getElementById(activeLinkid);
+
+//   if(activeLink==null){
+//     activeLink = document.getElementById("1");
+//   }
+
+//   list.forEach((item) => {
+//     item.classList.remove("hovered");
+//   });
+//   activeLink.classList.add("hovered");
+// }
+
+
+// function activeLink() {
+//   console.log("setting active link");
+//   sessionStorage.setItem("activeLinkID", this.id);
+// }
+
+// list.forEach((item) => item.addEventListener("click", activeLink));
+
 
 
 
