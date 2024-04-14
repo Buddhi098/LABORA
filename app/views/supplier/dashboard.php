a3994dc746caf80920fea939af42718f7f1f60fd
GitAvi001 <avindusridharmawardhana@gmail.com>
2024-04-14 14:14:33 +0530

    supplier dashboard completed

--- app/views/supplier/dashboard.php
+++ app/views/supplier/dashboard.php
@@ -4,48 +4,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta http-equiv="refresh" content="600; url=http://localhost/labora/user/logout"> -->
-    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/labassistant/dashboard.css'?>">
+    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/patientdashboard/dashboard.css'?>">
     <script src="<?php echo APPROOT.'/public/js/patientdashboard/patient.js';?>"></script>
     <!-- static icons -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <!-- annimation icons -->
     <script src="https://cdn.lordicon.com/lordicon-1.1.0.js"></script>
-    <title>Supplier dashboard</title>
+    <title>Lab assistant dashboard</title>
 </head>
 <body>
     <?php require_once 'components/nevbar.php' ?>
 
     <div class="container_1">
-        <div class="boxset-1">
-            <div class="box box-1">
-                <span>4</span><br>
-                <h5>Number of Approvals Received in a week</h5><br>
-                <p>Suppliers are waiting for the approvals of the quotations</p>
-            </div>
-            <div class="box box-2">
-                <span>10</span><br>
-                <h5>Number of Quotations send in a week</h5><br>
-                <p>Suppliers send the quotation details for the order</p>
-            </div>
-            <div class="box box-3">
-                <span>200$</span><br>
-                <h5>Total costs for sold Items</h5><br>
-                <p>Total cost will updated for the supplied items in a week</p>
-            </div>
-            <div class="box box-4">
-                <span>Syringe</span><br>
-                <h5>Most requested Item</h5><br>
-                <p>This box provides highly requested item in a week</p>
-            </div>
-        </div>
-    </div>
+        <div class="boxset_1">
 
-    <!--<div class="container_2">
-        <div class="boxset-2">
-            <div class="message box-1"></div>
-            <div class="notify box-1"></div>
+            <div class="box box_1">
+                <div class="text">
+                    <h5>Number of Orders</h5>
+                    <h1>30</h1>
+                    <p><i class="fa-solid fa-arrow-right" style="color: #ff0000;"></i>Total number of orders received form the inventory manager in previous month.</p>
+                </div>
+                <div class="icon">
+                <i class="fa-solid fa-bars"></i>
+                </div>
+            </div>
+
+            <div class="box box_2">
+                <div class="text">
+                    <h5>Number of Pending Orders</h5>
+                    <h1>10</h1>
+                    <p><i class="fa-solid fa-arrow-right" style="color: #ff0000;"></i>These orders are still pending check the availability on items and chemicals.</p>
+                </div>
+                <div class="icon">
+                <i class="fa-solid fa-hourglass-start"></i>
+                </div>
+            </div>
+
+            <div class="box box_2">
+                <div class="text">
+                    <h5>Number of Rejected Orders</h5>
+                    <h1>5</h1>
+                    <p><i class="fa-solid fa-arrow-right" style="color: #ff0000;"></i>Rejecting the orders because of unavailability of items or chemicals or lack of producers.</p>
+                </div>
+                <div class="icon">
+                <i class="fa-solid fa-xmark"></i>
+                </div>
+            </div>
+
+            <div class="box box_2">
+                <div class="text">
+                    <h5>Number of sent out quotations</h5>
+                    <h1>15</h1>
+                    <p><i class="fa-solid fa-arrow-right" style="color: #ff0000;"></i>Quotations are sent after confirmation of items or chemicals</p>
+                </div>
+                <div class="icon">
+                <i class="fa-solid fa-file-import"></i>
+                </div>
+            </div>
+
         </div>
-    </div>-->
+        
+
+    <script src="<?php echo APPROOT.'/public/js/patientdashboard/dashboard.js';?>"></script>
+
+
 </body>
 </html>
