3e842623b14166028f8a9db32198461c45bc9555
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-04-22 15:18:45 +0530

    change stylesheet

--- app/views/receptionist/refunded_appointment.php
+++ app/views/receptionist/refunded_appointment.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta http-equiv="refresh" content="600; url=http://localhost/labora/user/logout"> -->
-    <link rel="stylesheet" href="<?php echo APPROOT.'/public/css/receptionist/refunded_appointment.css'?>">
+    <link rel="stylesheet" href="<?php echo APPROOT . '/public/css/receptionist/refunded_appointment.css' ?>">
     <!-- static icons -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
@@ -12,80 +13,81 @@
     <script src="https://cdn.lordicon.com/lordicon-1.1.0.js"></script>
     <title>Receptionist dashboard</title>
 </head>
+
 <body>
     <?php require_once 'components/appointment_nevbar.php' ?>
     <div class="container_1">
 
-    <div class="table-container">
-        <h2><i class="fa-solid fa-calendar-check"></i>Appointments</h2>
-        <div class="search-container">
-        <input type="text" class="search-box" id="searchInput" placeholder="Search...">
-        <button class="search-button">Search</button>
-        </div>
+        <div class="table-container">
+            <h2><i class="fa-solid fa-calendar-check"></i>Appointments</h2>
+            <div class="search-container">
+                <input type="text" class="search-box" id="searchInput" placeholder="Search...">
+                <button class="search-button">Search</button>
+            </div>
 
-        <div class="filter-box">
-            <div class="filter-section">
-                <select class="filter-box">
-                <option value="all">All</option>
-                <option value="category1">Category 1</option>
-                <option value="category2">Category 2</option>
-                </select>
-                <button class="filter-button">Filter By ID</button>
+            <div class="filter-box">
+                <div class="filter-section">
+                    <select class="filter-box">
+                        <option value="all">All</option>
+                        <option value="category1">Category 1</option>
+                        <option value="category2">Category 2</option>
+                    </select>
+                    <button class="filter-button">Filter By ID</button>
+                </div>
+                <div class="filter-section">
+                    <select class="filter-box">
+                        <option value="all">All</option>
+                        <option value="category1">Category 1</option>
+                        <option value="category2">Category 2</option>
+                    </select>
+                    <button class="filter-button">Filter By Email</button>
+                </div>
             </div>
-            <div class="filter-section">
-                <select class="filter-box">
-                <option value="all">All</option>
-                <option value="category1">Category 1</option>
-                <option value="category2">Category 2</option>
-                </select>
-                <button class="filter-button">Filter By Email</button>
-            </div>
-        </div>
-        <table id="myTable">
-            <thead>
-                <th>Ref No</th>
-                <th>Patient Email</th>
-                <th>Test Category</th>
-                <th>Appointment Date</th>
-                <th>Refund Status</th>
-                <th>Refund Amount</th>
-                <th>Appointment Status</th>
-                <th>Payment Status</th>
-                <th>Action</th>
-            </thead >
-            <tbody>
-                <div class='table_body'>
-                    <?php
-                        if($data['appointment_data']){
-                            foreach($data['appointment_data'] as $appointment){
-                                if($appointment['refund_status'] == 'refunded'){
+            <table id="myTable">
+                <thead>
+                    <th>Ref No</th>
+                    <th>Patient Email</th>
+                    <th>Test Category</th>
+                    <th>Appointment Date</th>
+                    <th>Refund Status</th>
+                    <th>Refund Amount</th>
+                    <th>Appointment Status</th>
+                    <th>Payment Status</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <div class='table_body'>
+                        <?php
+                        if ($data['appointment_data']) {
+                            foreach ($data['appointment_data'] as $appointment) {
+                                if ($appointment['refund_status'] == 'refunded') {
                                     $str = 'disabled';
-                                }else{
+                                } else {
                                     $str = '';
                                 }
                                 echo "<tr>";
-                                echo "<td>".$appointment['Ref_No']."</td>";
-                                echo "<td>".$appointment['patient_email']."</td>";
-                                echo "<td>".$appointment['Test_Type']."</td>";
-                                echo "<td>".$appointment['Appointment_Date']."</td>";
-                                echo "<td>".$appointment['refund_status']."</td>";
-                                echo "<td>Rs. ".$appointment['cost'].".00</td>";
-                                echo "<td>".$appointment['Appointment_Status']."</td>";
-                                echo "<td>".$appointment['payment_status']."</td>";
+                                echo "<td>" . $appointment['Ref_No'] . "</td>";
+                                echo "<td>" . $appointment['patient_email'] . "</td>";
+                                echo "<td>" . $appointment['Test_Type'] . "</td>";
+                                echo "<td>" . $appointment['Appointment_Date'] . "</td>";
+                                echo "<td>" . $appointment['refund_status'] . "</td>";
+                                echo "<td>Rs. " . $appointment['cost'] . ".00</td>";
+                                echo "<td>" . $appointment['Appointment_Status'] . "</td>";
+                                echo "<td>" . $appointment['payment_status'] . "</td>";
                                 echo "<td><a><button class='viewbtn btn-0 btn-2' onclick=\"openWarningModal('" . $appointment['Id'] . "' , '" . $appointment['patient_email'] . "')\" " . $str . ">Pay Refund</button></a></td>";
                                 echo "</tr>";
                             }
-                        }else{
+                        } else {
                             echo '<tr><td colspan="100%" class="empty_msg">No data available in the table.</td></tr>';
                         }
-                    ?>
-                </div>
-            </tbody>
-        </table>
+                        ?>
+                    </div>
+                </tbody>
+            </table>
             <div class="pagination">
-            <h5 id="table_data"></h5>
-            <button onclick="previousPage()" >Previous</button>
-            <button onclick="nextPage()" id="next">Next</button>
+                <h5 id="table_data"></h5>
+                <button onclick="previousPage()">Previous</button>
+                <button onclick="nextPage()" id="next">Next</button>
             </div>
         </div>
     </div>
@@ -96,18 +98,18 @@
             <span class="close">&times;</span>
             <p>Are you sure you want to Pay Refund?</p>
             <div class="btn-container">
-            <button id="yesBtn">Yes</button>
-            <button id="noBtn">No</button>
-            <input type="hidden" value="" id="hidden_id">
-            <input type="hidden" value="" id="hidden_email">
+                <button id="yesBtn">Yes</button>
+                <button id="noBtn">No</button>
+                <input type="hidden" value="" id="hidden_id">
+                <input type="hidden" value="" id="hidden_email">
             </div>
         </div>
     </div>
 
-    <script src="<?php echo APPROOT.'/public/js/components/warningModal.js'?>"></script>
+    <script src="<?php echo APPROOT . '/public/js/components/warningModal.js' ?>"></script>
 
     <script>
-        function openWarningModal(id , email){
+        function openWarningModal(id, email) {
             console.log(id)
             console.log(email);
             document.getElementById('hidden_id').value = id;
@@ -115,16 +117,17 @@
             modal.style.display = "block";
         }
 
-        yesBtn.onclick = function() {
-          const baseLink = window.location.origin
-          const id = document.getElementById('hidden_id').value;
-          const email = document.getElementById('hidden_email').value;
-          const link = `${baseLink}/labora/receptionist/getRefundInovoice/${id}/${email}`
-          window.open(link , '_blank');
-          modal.style.display = "none";
+        yesBtn.onclick = function () {
+            const baseLink = window.location.origin
+            const id = document.getElementById('hidden_id').value;
+            const email = document.getElementById('hidden_email').value;
+            const link = `${baseLink}/labora/receptionist/getRefundInovoice/${id}/${email}`
+            window.open(link, '_blank');
+            modal.style.display = "none";
         }
     </script>
     <!-- import table javascript -->
-    <script src="<?php echo APPROOT.'/public/js/components/table.js'?>"></script>
+    <script src="<?php echo APPROOT . '/public/js/components/table.js' ?>"></script>
 </body>
+
 </html>
