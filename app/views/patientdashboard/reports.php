9456aa75c1696569a9b9eed9579566c4809ff52f
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-04-22 15:18:11 +0530

    update report view

--- app/views/patientdashboard/reports.php
+++ app/views/patientdashboard/reports.php
@@ -50,7 +50,7 @@
             <th>Ref No</th>
             <th>Test Type</th>
             <th>Date</th>
-            <th>Message</th>
+            <th>Report Status</th>
             <th>Actions</th>
         </thead >
         <tbody>
@@ -59,13 +59,26 @@
                     if(!empty($data)){
                         $reversedArray = array_reverse($data, true);
                         foreach ($reversedArray as $row) {
+                            if ($row['report_status'] == 'Pending') {
+                                $str = 'status-8';
+                            } else if ($row['report_status'] == 'Created') {
+                                $str = 'status-2';
+                            } else if ($row['report_status'] == 'Rejected') {
+                                $str = 'status-5';
+                            } else if ($row['report_status'] == 'Approved') {
+                                $str = 'status-3';
+                            } else if ($row['report_status'] == 'Review By MLT') {
+                                $str = 'status-2';
+                            } else if ($row['report_status'] == 'Completed') {
+                                $str = 'status-3';
+                            }
                             echo '<tr>
                             <td>'.$row['id'].'</td>
                             <td>'.$row['ref_no'].'</td>
                             <td>'.$row['test_type'].'</td>
                             <td>'.$row['date'].'</td>
-                            <td>'.$row['message'].'</td>
-                            <td><a href="http://localhost/uploads/'.$row['path'].'" >View</a> <a href="http://localhost/labora/PatientDashboard/deleteReport/'.$row['id'].'/'.$row['path'].'">delete</a></td>
+                            <td><div class="'.$str.'">'.$row['report_status'].'</div></td>
+                            <td><a href="' . APPROOT . "/PatientDashboard/viewReport/" . $row['ref_no'] . '" target=_blank"" ><button class="btn-0 btn-2">View</button></a> <a><button class="btn-0 btn-3" onclick=\'openModal("'.$row['ref_no'].'")\'>Delete</button></a></td>
                         </tr>';
                         }
                     }else{
@@ -84,6 +97,67 @@
             </div>
         </div>
     </div>
+
+    <!-- delete waring message -->
+    <div id="deleteModal" class="warning-modal">
+        <div class="warning-modal-content">
+            <span class="close">&times;</span>
+            <p>Are you sure you want to Delete Permanently?</p>
+            <div class="btn-container">
+                <button id="yesBtn">Yes</button>
+                <button id="noBtn">No</button>
+                <input type="hidden" value="" id="hidden_id">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- pop success & error messages -->
+    <!-- popup success messages -->
+    <div class="success-message-container" id="successMessage">
+        <div class="icon">
+            <lord-icon src="https://cdn.lordicon.com/guqkthkk.json" trigger="in" delay="15" state="in-reveal">
+            </lord-icon>
+        </div>
+        <p id="success_msg"> Success! Add New Medical Test.</p>
+        <span class="close-button" onclick="hideSuccessMessage()">×</span>
+    </div>
+
+    <div class="error-message-container" id="ErrorMessage">
+        <div class="icon">
+            <lord-icon src="https://cdn.lordicon.com/akqsdstj.json" trigger="in" delay="15" state="in-reveal">
+            </lord-icon>
+        </div>
+        <p id="error_msg">Error! Your action was failed.</p>
+        <span class="close-button" onclick="hideSuccessMessage()">×</span>
+    </div>
+
+    <!-- for showing sucess message -->
+    <script>
+        window.onload = showMessage();
+
+        function showMessage() {
+            let success = '<?php echo isset($_SESSION["success_msg"]) ? json_encode($_SESSION["success_msg"]) : ""; ?>';
+            <?php unset($_SESSION["success_msg"]); ?>;
+            if (success.trim() !== "") {
+                console.log(success);
+                document.getElementById('success_msg').innerHTML = success;
+                showSuccessMessage();
+            }
+        }
+    </script>
+
+    <!-- waring modal -->
+    <script src="<?php echo APPROOT . '/public/js/components/warningModal.js' ?>"></script>
+
+    <script>
+        let yesBtn1 = document.getElementById('yesBtn');
+        yesBtn1.addEventListener('click' , ()=>{
+            let ref_no= document.getElementById('hidden_id').value;
+            console.log('zczx');
+            window.location.href = "<?php echo URLROOT.'/PatientDashboard/deleteReport/'?>" + ref_no;
+        })
+    </script>
 
     <script src="<?php echo APPROOT.'/public/js/components/table.js'?>"></script>
 </body>
