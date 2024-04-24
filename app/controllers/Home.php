4cea817c0cb66652449eb9414fd4e9b7d874fc5a
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-04-24 14:34:39 +0530

    fix bugs

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,9 +8,21 @@
             $this->view("Home" , []);
         }
 
-        public function getService(){
-            $result = $this->md_testtype->getRow();
-            exit(json_encode($result));
+        public function getService($id , $table_id=0){
+            if($id == 0){
+                $result = $this->md_testtype->getRowNew();
+                $table_id_ = $result['table_id'];
+                $result = $result['result'];
+            }else{
+                $result = $this->md_testtype->getRowNew();
+                $table_id_ = $table_id;
+                $result = $result['result'];
+            }
+            $preparation = $this->md_testtype->getPreparationsById($table_id_);
+            $data['result'] = $result;
+            $data['preparation'] = $preparation;
+
+            exit(json_encode($data));
         }
     }
 ?>
